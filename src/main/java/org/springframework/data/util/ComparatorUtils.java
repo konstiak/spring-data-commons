1d8bc14f701670619618eb4a49dfd6ae443ee38c
Martin Konstiak <konstiak@isw.sk>
2020-03-10 22:46:14 +0100

    Added javadoc and fixed tests

--- src/main/java/org/springframework/data/util/ComparatorUtils.java
+++ src/main/java/org/springframework/data/util/ComparatorUtils.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2012-2020 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.springframework.data.util;
 
 import lombok.experimental.UtilityClass;
@@ -18,7 +33,7 @@ import static java.util.Optional.ofNullable;
 import static org.springframework.beans.BeanUtils.getPropertyDescriptor;
 
 /**
- * Reflection based converter for {@link Sort} to make it applicable as Comparator.
+ * Converter for {@link Sort} to make it applicable as Comparator based on reflection.
  *
  * @author Martin Konstiak
  */
@@ -26,6 +41,15 @@ import static org.springframework.beans.BeanUtils.getPropertyDescriptor;
 @UtilityClass
 public class ComparatorUtils {
 
+	/**
+	 * Converts {@link Sort} type to appropriate {@link Comparator} which can compare provided entities.
+	 * Converts all provided orders to separate comparator and joins them into one by thenComparing operation.
+	 * Null values have precedence in case of ascending direction.
+	 *
+	 * @param sort sort to convert
+	 * @param <T> class to which the comparator can be applied
+	 * @return generated comparator
+	 */
 	public static <T> Comparator<T> comparatorOf(Sort sort) {
 		return Comparator.nullsFirst(sort.get().map(ComparatorUtils::comparatorOf)
 										 .map(Comparator::nullsFirst)
@@ -33,7 +57,16 @@ public class ComparatorUtils {
 										 .orElse(comparing(e -> 0)));
 	}
 
-	private static <T, U extends Comparable<? super U>> Comparator<T> comparatorOf(Sort.Order order) {
+	/**
+	 * Converts {@link Sort.Order} type to appropriate {@link Comparator} which can compare provided entities.
+	 * Null values have precedence in case of ascending direction.
+	 *
+	 * @param order order to convert
+	 * @param <T> class to which the comparator can be applied
+	 * @param <U> class of property specified in order
+	 * @return generated comparator
+	 */
+	public static <T, U extends Comparable<? super U>> Comparator<T> comparatorOf(Sort.Order order) {
 		Function<T, U> keyExtractor = keyExtractor(order.getProperty());
 		Comparator<T> comparator = comparing(keyExtractor, nullSafeComparator());
 		return order.isAscending() ? comparator : comparator.reversed();
@@ -55,9 +88,7 @@ public class ComparatorUtils {
 	}
 
 	private static List<String> parsePropertiesPath(String propertiesPath) {
-		return ofNullable(StringUtils.split(propertiesPath, "."))
-				.map(Arrays::asList)
-				.orElse(Collections.emptyList());
+		return Arrays.asList(StringUtils.delimitedListToStringArray(propertiesPath, "."));
 	}
 
 	private static <T extends Comparable<? super T>> Comparator<T> nullSafeComparator() {
--- src/test/java/org/springframework/data/util/ComparatorUtilsTests.java
+++ src/test/java/org/springframework/data/util/ComparatorUtilsTests.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2014-2020 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.springframework.data.util;
 
 import lombok.Builder;
@@ -17,7 +32,7 @@ import static org.assertj.core.api.Assertions.assertThat;
  *
  * @author Martin Konstiak
  */
-class ComparatorUtilsTest {
+public class ComparatorUtilsTests {
 
 	@Test
 	public void orderedBySimpleType() {
@@ -40,7 +55,7 @@ class ComparatorUtilsTest {
 				Entity.builder().subEntity(Entity.builder().name("B").build()).build()
 		);
 
-		Comparator<Entity> comparator = ComparatorUtils.comparatorOf(Sort.by(Sort.Direction.DESC, "subEntity.subName"));
+		Comparator<Entity> comparator = ComparatorUtils.comparatorOf(Sort.by(Sort.Direction.DESC, "subEntity.name"));
 		List<Entity> sorted = list.stream().sorted(comparator).collect(Collectors.toList());
 
 		assertThat(sorted.get(0).getSubEntity().getName()).isEqualTo("B");
@@ -57,8 +72,8 @@ class ComparatorUtilsTest {
 		Comparator<Entity> comparator = ComparatorUtils.comparatorOf(Sort.unsorted());
 		List<Entity> sorted = list.stream().sorted(comparator).collect(Collectors.toList());
 
-		assertThat(sorted.get(0).getName()).isEqualTo("1");
-		assertThat(sorted.get(1).getName()).isEqualTo("2");
+		assertThat(sorted.get(0).getName()).isEqualTo("2");
+		assertThat(sorted.get(1).getName()).isEqualTo("1");
 	}
 
 	@Test
